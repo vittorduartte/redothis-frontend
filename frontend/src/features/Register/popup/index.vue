1ed19ac3b90b28acacc6a2f9f51b4f60315ecdeb
Mateus Vitor <mateusriograndense@gmail.com>
2020-11-09 09:53:13 -0300

    Validação de formulário de registro. Correção de margens para error notification sem barra de rolagem.

--- frontend/src/features/Register/popup/index.vue
+++ frontend/src/features/Register/popup/index.vue
@@ -4,43 +4,69 @@
     :color="color"
     background-color="rgba(98,39,255,.65)"
     :active.sync="registerUserPopupHome"
+    @close="resetForm()"
   >
     <vs-row vs-type="flex" vs-justify="flex-start" vs-align="center"> </vs-row>
     <vs-row vs-type="flex" vs-justify="space-around">
       <vs-col vs-w="11">
         <vs-input
           class="register-input"
+          :danger="$v.nome.$error"
+          danger-text="Por favor, preencha o campo de nome corretamente."
+          :color="$v.nome.$error ? 'danger':'primary'"
           label="Nome:"
           placeholder="John Doe"
+          v-model="nome"
+          @change="$v.nome.$touch()"
         ></vs-input>
       </vs-col>
       <vs-col vs-w="5">
         <vs-input
           class="register-input"
+          :danger="$v.email.$error"
+          danger-text="Por favor, preencha o campo de email corretamente."
+          :color="$v.email.$error ? 'danger':'primary'"
           label="E-mail:"
           placeholder="email@exemplo.com"
+          v-model="email"
+          @change="$v.email.$touch()"
         ></vs-input>
       </vs-col>
       <vs-col vs-w="5">
         <vs-input
           class="register-input"
+          :danger="$v.confemail.$error"
+          danger-text="Este email não condiz com o e-mail principal."
+          :color="$v.confemail.$error ? 'danger':'primary'"
           label="Confirmar e-mail:"
           placeholder="email@exemplo.com"
+          v-model="confemail"
+          @change="$v.confemail.$touch()"
         ></vs-input>
       </vs-col>
       <vs-col vs-w="5">
         <vs-select
           class="register-input"
+          :danger="$v.instituicao.$error"
+          danger-text="Por favor, selecione uma instituição."
+          :color="$v.instituicao.$error ? 'danger':'primary'"
           label="Instituição:"
           placeholder="Selecione uma instituição"
+          v-model="instituicao"
+          @change="$v.instituicao.$touch()"
         >
         </vs-select>
       </vs-col>
       <vs-col vs-w="5">
         <vs-select
           class="register-input"
+          :danger="$v.titulo.$error"
+          danger-text="Por favor, selecione um título."
+          :color="$v.titulo.$error ? 'danger':'primary'"
           label="Título:"
           placeholder="Selecione um título"
+          v-model="titulo"
+          @change="$v.titulo.$touch()"
         >
           <vs-select-item
             :value="item.value"
@@ -77,6 +103,9 @@
       <vs-col vs-w="5">
         <vs-select
           class="register-input"
+          :danger="$v.curso.$error"
+          danger-text="Por favor, selecione um curso."
+          :color="$v.curso.$error ? 'danger':'primary'"
           label="Curso:"
           placeholder="Selecione um curso"
         >
@@ -84,26 +113,57 @@
       </vs-col>
     </vs-row>
     <vs-row vs-type="flex" vs-justify="space-around">
-        <vs-col vs-w="5">
-            <vs-input class="register-input" label="Senha:" placeholder="••••••••••" type="password"></vs-input>
-        </vs-col>
-        <vs-col vs-w="5">
-            <vs-input class="register-input" label="Confirmar senha:" placeholder="••••••••••" type="password"></vs-input>
-        </vs-col>
+      <vs-col vs-w="5">
+        <vs-input
+          class="register-input"
+          :danger="$v.senha.$error"
+          danger-text="Por favor, preencha o campo de senha."
+          :color="$v.senha.$error ? 'danger':'primary'"
+          label="Senha:"
+          placeholder="••••••••••"
+          type="password"
+          v-model="senha"
+          @change="$v.senha.$touch()"
+        ></vs-input>
+      </vs-col>
+      <vs-col vs-w="5">
+        <vs-input
+          class="register-input"
+          :danger="$v.nome.$error"
+          danger-text="Esta senha não condiz com a escolhida."
+          :color="$v.nome.$error ? 'danger':'primary'"
+          label="Confirmar senha:"
+          placeholder="••••••••••"
+          type="password"
+          v-model="confsenha"
+          @change="$v.confsenha.$touch()"
+        ></vs-input>
+      </vs-col>
     </vs-row>
     <vs-row vs-type="flex" vs-justify="center">
-        <vs-col vs-type="flex" vs-justify="flex-start" vs-w="10">
-            <vs-checkbox class="register-terms" icon="thumb_up_alt">Eu aceito todos os termos da <a href="#">política de uso</a> da empresa.</vs-checkbox>
-        </vs-col>
-        <vs-col vs-w="10">
-            <vs-button type="gradient" :color="color" :gradient-color-secondary="colory" class="register-button">Vamos começar o trabalho!</vs-button>
-        </vs-col>
+      <vs-col vs-type="flex" vs-justify="flex-start" vs-w="10">
+        <vs-checkbox v-model="termos" class="register-terms" icon="thumb_up_alt"
+          >Eu aceito todos os termos da <a href="#">política de uso</a> da
+          empresa.</vs-checkbox
+        >
+      </vs-col>
+      <vs-col vs-w="10">
+        <vs-button
+          type="gradient"
+          @click="registerUser()"
+          :color="color"
+          :gradient-color-secondary="colory"
+          class="register-button"
+          >Vamos começar o trabalho!</vs-button
+        >
+      </vs-col>
     </vs-row>
   </vs-popup>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
+import { required, sameAs } from "vuelidate/lib/validators";
 
 export default {
   data() {
@@ -116,10 +176,44 @@ export default {
         { text: "Mestrado", value: 3 },
         { text: "Doutorado", value: 4 },
       ],
+      nome:'',
+      email:'',
+      confemail:'',
+      instituicao:{},
+      titulo:{},
+      tipoDeUsuario:'',
+      curso:{},
+      senha:'',
+      confsenha:'',
+      termos:false,
     };
+  },
+  validations:{
+    nome:{required},
+    email:{required},
+    confemail: {
+      confirmEmail: sameAs('email')
+    },
+    senha:{required},
+    confsenha: {
+      confirmPassword: sameAs('senha')
+    },
+    instituicao:{required},
+    titulo:{required},
+    curso:{required},
   },
   methods: {
     ...mapActions("Register", ["openPopupUserRegister"]),
+    registerUser(){
+      if(this.$v.$invalid){
+        this.$v.$touch();
+      } else {
+        return false
+      }
+    },
+    resetForm(){
+      this.$v.$reset();
+    }
   },
   computed: {
     ...mapState("Register", ["popupUserRegister"]),
@@ -137,7 +231,7 @@ export default {
 
 <style scoped>
 .register-input {
-  margin: 5px auto;
+  margin: 2px auto;
   width: 100%;
 }
 
@@ -153,12 +247,12 @@ li {
   margin: 15px 0px 0px 0px;
 }
 
-.register-terms{
-    margin-top: 20px;
+.register-terms {
+  margin-top: 10px;
 }
 
-.register-button{
-    width:100%;
-    margin: 20px 0;
+.register-button {
+  width: 100%;
+  margin: 15px 0;
 }
 </style>
